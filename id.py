f371f6a0b7fad53a167399945773973092856b95
lazarevaak <74897697+lazarevaak@users.noreply.github.com>
2022-05-22 12:46:22 +0300

    Add files via upload

---
+++ id.py
@@ -0,0 +1,19 @@
+me = 1415300064
+a = 2040375134
+
+
+
+def director():
+    return 0
+
+
+def secretary():
+    return 1415300064
+
+
+def assistant():
+    return 0
+
+
+me = 1415300064
+a = 2040375134
